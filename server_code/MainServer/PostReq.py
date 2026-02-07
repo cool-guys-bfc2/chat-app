cc7b21c42ad5932b91b86360d558a1a1101cea8e
lamarasingham@icloud.com <lamarasingham@icloud.com>
2026-02-07 14:20:14 +0000

    Created MainServer.PostReq; Edited settings

---
+++ server_code/MainServer/PostReq.py
@@ -0,0 +1,32 @@
+import anvil.secrets
+import anvil.stripe
+import anvil.files
+from anvil.files import data_files
+import anvil.facebook.auth
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.route('/update')
+def route():
+  x=app_tables.versioning.search(Version name='V2')
+  for i in x:
+    app_tables.versioning.get(Version name='V2')['Version Number']+=0.1
+    app_table.versioning.get(Version name='V2')['Time']=dat
